821435e2519fef11e45145744e015f06b82e2898
Shukri Adams <shukri.adams@digitaslbi.com>
2016-10-25 14:04:10 +0200

    moving in

---
+++ tasks/moduleViaBower.js
@@ -0,0 +1,57 @@
+/*
+
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+ 
+	var pluginName = 'moduleViaBower',
+		globsync = require('glob-whatev'),
+		path = require('path'),
+		fs = require('fs'),
+		jf = require('jsonfile');
+
+	grunt.registerTask(pluginName, '', function() {
+	    
+	    var fs = require('fs-extra'),
+            cwd = process.cwd(),
+	    	config = grunt.config(pluginName);
+
+	    // verify settings
+	    if (!config){
+	    	grunt.log.error(pluginName + ' could not find expected config item "' + pluginName +'".');
+	    }
+
+    	for (var i = 0 ; i < config.length ; i ++){
+
+	        var configItem = config[i],
+				targetPath = path.join(cwd, configItem.target),
+	        	sourcePath = path.join(cwd, configItem.src),
+    	        isClone = directoryExists(path.join(targetPath, '.git'));
+
+	        if (!isClone){
+	            fs.ensureDirSync(targetPath);
+	            fs.copySync(sourcePath, targetPath);
+	            console.log('Copied module to ' + configItem.target  + '.');
+	        } else {
+	            console.log('Git found at ' + configItem.target + ', skipping copy.');
+	        }
+    	}
+
+        function directoryExists(path)
+        {
+            var fs = require('fs');
+
+            try
+            {
+                return fs.statSync(path).isDirectory();
+            }
+            catch (err)
+            {
+                return false;
+            }
+        }
+		
+	});
+};
